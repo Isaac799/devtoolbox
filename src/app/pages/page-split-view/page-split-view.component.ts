81832afd1f810b66cfaf87d494c1a156a1fc19c7
Isaac Lewis <mail@isaaclewis.me>
2025-02-03 18:05:43 -0600

    css tweaks

--- src/app/pages/page-split-view/page-split-view.component.ts
+++ src/app/pages/page-split-view/page-split-view.component.ts
@@ -2,18 +2,18 @@ import {Component, ElementRef, HostListener, inject, OnInit, ViewChild} from '@a
 import {PageCodeOutputComponent} from '../page-code-output/page-code-output.component'
 import {PageTextEditorComponent} from '../page-text-editor/page-text-editor.component'
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout'
-import { Router } from '@angular/router'
+import {Router} from '@angular/router'
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
     selector: 'app-page-split-view',
-    imports: [PageCodeOutputComponent, PageTextEditorComponent],
+    imports: [PageCodeOutputComponent, PageTextEditorComponent, MatIconModule],
     templateUrl: './page-split-view.component.html',
     styleUrl: './page-split-view.component.scss'
 })
 export class PageSplitViewComponent implements OnInit {
     private readonly breakpointObserver = inject(BreakpointObserver)
     private readonly router = inject(Router)
-
 
     @ViewChild('divider') divider: ElementRef | undefined = undefined
     @ViewChild('leftPanel') leftPanel: ElementRef | undefined = undefined
@@ -24,12 +24,9 @@ export class PageSplitViewComponent implements OnInit {
     i = 0
 
     ngOnInit(): void {
-      if (
-        !this.breakpointObserver.isMatched(Breakpoints.Large) &&
-        !this.breakpointObserver.isMatched(Breakpoints.XLarge) 
-      ) {
-        this.router.navigate([''])
-      }
+        if (!this.breakpointObserver.isMatched(Breakpoints.Large) && !this.breakpointObserver.isMatched(Breakpoints.XLarge)) {
+            this.router.navigate([''])
+        }
     }
 
     // Start dragging
