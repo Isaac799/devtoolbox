5d0be239b3d74577736c9981ccbc93b8af640084
Isaac Lewis <mail@isaaclewis.me>
2025-10-18 13:04:10 -0500

    add: upper acronym str conv

--- internal/strgen/strgen.go
+++ internal/strgen/strgen.go
@@ -1,2 +1,88 @@
 // Package strgen is designed to generate strings from a model
 package strgen
+
+import (
+	"strings"
+
+	"github.com/iancoleman/strcase"
+)
+
+func renderCamel(s string) string {
+	return strcase.ToLowerCamel(s)
+}
+
+func renderSnake(s string) string {
+	return strcase.ToSnake(s)
+}
+
+func renderKebab(s string) string {
+	return strcase.ToKebab(s)
+}
+
+func renderPascal(s string) string {
+	return strcase.ToCamel(s)
+}
+
+// upAcronym tries to keep uppercase acronyms to aid in convention following,
+// so "fooId" -> "fooID". It takes a string, and the convention to be used
+// around the casing.
+func upAcronym(s string, convention func(string) string) string {
+	s = strcase.ToSnake(s)
+
+	words := strings.Split(s, "_")
+	acronyms := []string{
+		"id", "html", "css", "js", "eol", "eof", "dob", "ttl",
+	}
+
+	type sChange struct {
+		s       string
+		changed bool
+	}
+
+	changes := make([]sChange, 0, len(words))
+
+wordHunt:
+	for _, word := range words {
+		for _, ac := range acronyms {
+			if word != ac {
+				continue
+			}
+			fixed := strings.ReplaceAll(word, ac, strings.ToUpper(ac))
+			changes = append(changes, sChange{s: fixed, changed: true})
+			continue wordHunt
+		}
+		changes = append(changes, sChange{s: word, changed: false})
+	}
+
+	final := make([]string, 0, len(words))
+	stack := make([]string, 0, len(words))
+
+	var processStack = func() {
+		if len(stack) == 0 {
+			return
+		}
+		s := convention(strings.Join(stack, "_"))
+		final = append(final, s)
+		stack = make([]string, 0, len(words))
+	}
+
+	for _, change := range changes {
+		if change.changed {
+			processStack()
+			final = append(final, change.s)
+			continue
+		}
+		stack = append(stack, change.s)
+	}
+	processStack()
+
+	return strings.Join(final, "")
+}
+
+func renderCamelUA(s string) string {
+	return upAcronym(s, strcase.ToLowerCamel)
+}
+
+func renderPascalUA(s string) string {
+	return upAcronym(s, strcase.ToCamel)
+}
